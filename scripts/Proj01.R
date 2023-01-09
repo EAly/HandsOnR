45dbc43cb9ce095254f1562b80cac20382216e5a
ealy <ebrahim.a.aly@gmail.com>
2023-01-09 14:48:07 +1100

    wrap proj 1

--- scripts/Proj01.R
+++ scripts/Proj01.R
@@ -28,3 +28,17 @@ roll_sum2 <- function(bones = 1:6) {
 roll_sum2(bones = 1:4)
 roll_sum2(1:10)
 roll_sum2()
+
+# repeat a command in R for n times using the replicate function
+
+replicate(10, roll_sum2()) # replicate roll_sum2 for 10 times
+
+# create a weighted die roll
+
+unfair_roll <- function(die = 1:6) {
+  dice <- sample(die, size = 2, replace = TRUE,
+         prob = c(1/8, 1/8, 1/8, 1/8, 1/8, 3/8))
+  sum(dice)
+}
+
+replicate(10, unfair_roll())
